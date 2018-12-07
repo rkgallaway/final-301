857294e9c4aefb390bde75a01bb7559bdb78b11a
Jacob Anderson <1anderson2jacob@gmail.com>
2018-12-07 13:47:20 -0800

    automatically saving to 'lastsearched' table after deleting its contents now working

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 'use strict';
-//+++++++++++++++dependencies+++++++++++++++++++
-console.log('hi');
+
+//+++++++++++++++ DEPENDENCIES +++++++++++++++++++
+
 const express = require('express');
-//const superagent = require('superagent');
 const pg = require('pg');
 const fetch = require('node-fetch');
-//const util = require('util');
-// const cors =  require('cors');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+//const superagent = require('superagent');
+//const util = require('util');
+// const cors =  require('cors');
 
 //databse setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 console.log('~~~~~~~~~~~~');
+
 //set view engine for templating
 app.set('view engine', 'ejs');
 
@@ -23,40 +25,48 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(express.urlencoded( {extended: true} ));
 
-//+++++++++++++++routes+++++++++++++++++++
+//+++++++++++++++ ROUTES +++++++++++++++++++
 
 //route  for home view
 app.get('/', getHome)
 app.post('/results', getCompanyDomain);
-app.get('new-search')
-//
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //handler for POST request / searches
 
-// //+++++++++++++++models++++++++++++++++
+//+++++++++++++++ MODELS ++++++++++++++++
+
 function Company(fullContact, clearBit) {
-  this.tableName = 'lastSearched';
+  this.tableName = 'lastsearched';
   this.companyName = fullContact.name;
   this.founded = fullContact.founded;
   this.size = fullContact.employees;
   this.leaders = fullContact.dataAddOns ? fullContact.dataAddOns.name : 'unknown leaders';
-  this.product = 'products'
-  this.clients = 'clients'
-  this.mission = 'missions'
-  this.contacts = 'contacts'
-  this.location = 'location'
+  this.product;
+  this.clients;
+  this.mission;
+  this.contacts;
+  this.location;
   this.domain = clearBit.domain
   this.logo = clearBit.logo
+  this.notes; //needs populated w/ sql notes
 }
 
 function saveCompany(company) {
-  const SQL = `INSERT INTO ${company.tableName} (companyName, founded, size, leaders, product, clients, mission, contacts, location, domain, logo, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`;
-  const values = [company.companyName, company.founded, company.size, company.leaders, company.product, company.clients, company.mission, company.contacts, company.location, company.domain, company.logo];
+  const values = [company.companyName, company.founded, company.size, company.leaders, company.product, company.clients, company.mission, company.contacts, company.location, company.domain, company.logo,company.notes];
+
+  let SQL = `DELETE FROM ${company.tableName};`;
+  client.query(SQL);
+
+  SQL =`INSERT INTO ${company.tableName} (companyName, founded, size, leaders, product, clients, mission, contacts, location, domain, logo, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`;
+
   client.query(SQL,values);
 }
-// //++++++++++++++helpers+++++++++++++++
-// //on page load
+
+//++++++++++++++ HELPERS +++++++++++++++
+
+//on page load
 function getHome(request, response){
   let SQL = 'SELECT * from savedCompanies;';
   return client.query(SQL)
@@ -68,12 +78,29 @@ function getHome(request, response){
   //load savedComppanies
 }
 
-// //++++++++++++++++handlers+++++++++++++++++
+//++++++++++++++++ HANDLERS +++++++++++++++++
 
-// //error handler
+//error handler
 function handleError(err, res){
   console.error(err);
   res.render('error.ejs', {error: 'Error recieved'});
+}
+
+function getCompanyDomain(request, response){
+  // console.log(request.body.searchTerm);
+  fetch(`https://company.clearbit.com/v1/domains/find?name=${request.body.searchTerm}`, { //need to get search results
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.CLEARBIT_API_KEY}` //add to .env
+    }
+
+  }).then(function(res) {
+    return res.json();
+  }).then(function(json) {
+    console.log('json: (should return clearbit object)', json)
+    getCompanyInfo(request, response, json);
+  })
+    .catch( (error) => handleError(error) );
 }
 
 function getCompanyInfo(request, response, json){ // we passed in trying to find the dmoain we will get
@@ -98,23 +125,6 @@ function getCompanyInfo(request, response, json){ // we passed in trying to find
     .then(results => {
       return response.render('results', {searchResults: results})})
 
-    .catch( (error) => handleError(error) );
-}
-
-function getCompanyDomain(request, response){
-  // console.log(request.body.searchTerm);
-  fetch(`https://company.clearbit.com/v1/domains/find?name=${request.body.searchTerm}`, { //need to get search results
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${process.env.CLEARBIT_API_KEY}` //add to .env
-    }
-
-  }).then(function(res) {
-    return res.json();
-  }).then(function(json) {
-    console.log('json: (should return clearbit object)', json)
-    getCompanyInfo(request, response, json);
-  })
     .catch( (error) => handleError(error) );
 }
 
