3eec7a53842609dc70fb0e3c8f04ee9ac87a7d03
Jacob Anderson <1anderson2jacob@gmail.com>
2018-12-06 19:05:17 -0800

    We're successfully loading companyName, founded, size, domain, logo. 90% of the way to saving

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
-//dependencies
+//+++++++++++++++dependencies+++++++++++++++++++
 console.log('hi');
 const express = require('express');
-const superagent = require('superagent');
+//const superagent = require('superagent');
 const pg = require('pg');
 const fetch = require('node-fetch');
+//const util = require('util');
 // const cors =  require('cors');
 const app = express();
 require('dotenv').config();
@@ -27,27 +28,33 @@ app.use(express.urlencoded( {extended: true} ));
 //route  for home view
 app.get('/', getHome)
 app.post('/results', getCompanyDomain);
-
+app.get('new-search')
 //
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //handler for POST request / searches
 
 // //+++++++++++++++models++++++++++++++++
-function Company(info) {
-  this.companyName = info
-  this.founded = info
-  this.size = info
-  this.leaders = info
-  this.product = info
-  this.clients = info
-  this.mission = info
-  this.contacts = info
-  this.location = info
-  this.domain = info
-  this.log = info
+function Company(fullContact, clearBit) {
+  this.tableName = 'lastSearched';
+  this.companyName = fullContact.name;
+  this.founded = fullContact.founded;
+  this.size = fullContact.employees;
+  this.leaders = fullContact.dataAddOns ? fullContact.dataAddOns.name : 'unknown leaders';
+  this.product = 'products'
+  this.clients = 'clients'
+  this.mission = 'missions'
+  this.contacts = 'contacts'
+  this.location = 'location'
+  this.domain = clearBit.domain
+  this.logo = clearBit.logo
 }
 
+function saveCompany(company) {
+  const SQL = `INSERT INTO ${company.tableName} (companyName, founded, size, leaders, product, clients, mission, contacts, location, domain, logo, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`;
+  const values = [company.companyName, company.founded, company.size, company.leaders, company.product, company.clients, company.mission, company.contacts, company.location, company.domain, company.logo];
+  client.query(SQL,values);
+}
 // //++++++++++++++helpers+++++++++++++++
 // //on page load
 function getHome(request, response){
@@ -58,27 +65,59 @@ function getHome(request, response){
     })
     .catch( (error) => handleError(error) );
 
-
   //load savedComppanies
 }
 
 // //++++++++++++++++handlers+++++++++++++++++
-
-// //on page load
-// function getHome(request, response){
-//   let SQL = 'SELECT * from savedCompanies;';
-//   return client.query(SQL)
-//     .then( (results) => {
-//       response.render('/', {showSavedCompanies: results.rows});
-//     })
-//     .catch( (error) => handleError(error) );
-// }
 
 // //error handler
 function handleError(err, res){
   console.error(err);
   res.render('error.ejs', {error: 'Error recieved'});
 }
+
+function getCompanyInfo(request, response, json){ // we passed in trying to find the dmoain we will get
+  fetch('https://api.fullcontact.com/v3/company.enrich',{
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${process.env.FULLCONTACT_API_KEY}` //add to .env
+    },
+    body: JSON.stringify({
+      'domain': json.domain //we added, not sure where it lives yet
+    })
+  })
+    .then(function(res) {
+      return res.json();
+    }) 
+    .then(apiResponse => {
+      //console.log(apiResponse)
+      const newCompany = new Company(apiResponse, json)
+      saveCompany(newCompany)
+      return newCompany
+    })
+    .then(results => {
+      return response.render('results', {searchResults: results})})
+
+    .catch( (error) => handleError(error) );
+}
+
+function getCompanyDomain(request, response){
+  // console.log(request.body.searchTerm);
+  fetch(`https://company.clearbit.com/v1/domains/find?name=${request.body.searchTerm}`, { //need to get search results
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${process.env.CLEARBIT_API_KEY}` //add to .env
+    }
+
+  }).then(function(res) {
+    return res.json();
+  }).then(function(json) {
+    console.log('json: (should return clearbit object)', json)
+    getCompanyInfo(request, response, json);
+  })
+    .catch( (error) => handleError(error) );
+}
+
 // function getCompany(request, response){
 
 // }
@@ -92,52 +131,23 @@ function handleError(err, res){
 //     .catch(error => handleError(error, response))
 // }
 // // reference above
-function getCompanyInfo(response){ // we passed in trying to find the dmoain we will get
-  fetch('https://api.fullcontact.com/v3/company.enrich',{
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${process.env.FULLCONTACT_API_KEY}` //add to .env
-    },
-    body: JSON.stringify({
-      'domain': response.domain //we added, not sure where it lives yet
-    })
-  }).then(function(res) {
-    return res.json();
-  })
-    .then(results => response.render('results', {searchResults: results}))
-    .catch( (error) => handleError(error) );
 
 
-}
+// //on page load
+// function getHome(request, response){
+//   let SQL = 'SELECT * from savedCompanies;';
+//   return client.query(SQL)
+//     .then( (results) => {
+//       response.render('/', {showSavedCompanies: results.rows});
+//     })
+//     .catch( (error) => handleError(error) );
+// }
 
-function getCompanyDomain(request, response){
-  console.log('request' + request);
-  fetch(`https://company.clearbit.com/v1/domains/find?name=${request}`, { //need to get search results
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${process.env.CLEARBIT_API_KEY}` //add to .env
-    }
+// Company.prototype = {
+//   save: function() {
+//     const SQL = `INSERT INTO ${this.tableName} (companyName, founded, size, leaders, product, clients, mission, contacts, location, domain, logo, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`;
+//     const values = [this.companyName, this.founded, this.size, this.leaders, this.product, this.clients, this.mission, this.contacts, this.location, this.domain, this.logo];
 
-  }).then(function(res) {
-    return res.json();
-  }).then(function(json) {
-    console.log('json: ' + json);
-    getCompanyInfo(json);
-  })
-    .catch( (error) => handleError(error) );
-
-
-}
-
-
-
-
-
-
-// new search
-
-// first we need location optained
-
-// give location to clearbit to get domain name
-
-// use
+//     client.query(SQL,values);
+//   }
+// }
