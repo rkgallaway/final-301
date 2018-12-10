0ebd480b225fb99052fa1319ed506838be60b2c1
tyler <tyler.r.hood@gmail.com>
2018-12-10 11:10:01 -0800

    added toggled feature, and fixed nav

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,22 @@ $('.hamburger').on('click', () => {
     height: 'toggle',
     color: 'green',
   },100);
-  
+
 });
+
+function toggleFunction() {
+  let $class = $(".hideStuff");
+  let $plus = $(".plus");
+  let $minus = $(".minus");
+  if ($class.css("display") === 'flex') {
+    $class.css("display", "none");
+    $plus.css("display", "flex");
+    $minus.css("display", "none");
+  } else {
+    $class.css("display", "flex");
+    $plus.css("display", "none");
+    $minus.css("display", "flex");
+  }
+}
+
+
